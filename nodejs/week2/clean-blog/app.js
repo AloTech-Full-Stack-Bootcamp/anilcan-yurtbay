70a2d264ab7a72a43f9d74f6fa6615a64e7ddd1b
Anılcan Yurtbay <anilcanyurtbay@hotmail.com>
2021-11-01 14:14:23 +0300

    models create & mongo settings & post data

--- nodejs/week2/clean-blog/app.js
+++ nodejs/week2/clean-blog/app.js
@@ -1,13 +1,27 @@
 const express = require('express');
+const mongoose = require('mongoose');
+
 const ejs = require('ejs');
-const app = express();
+const Blog = require('./models/Blog');
+
 const port = 5000;
+
+// EXPRESS START
+const app = express();
+
+// DB CONNECTION
+mongoose.connect('mongodb://localhost/cleanblog-test-db',{
+    useNewUrlParser:true,
+    useUnifiedTopology:true
+})
 
 // TEMPLATE ENGINE
 app.set("view engine","ejs")
 
 // MIDDLEWARE
 app.use(express.static('public'))
+app.use(express.urlencoded({extended:true}))
+app.use(express.json())
 
 // ROUTES
 app.get('/',(req,res)=>{
@@ -19,6 +33,12 @@ app.get('/about',(req,res)=>{
 app.get('/add',(req,res)=>{
     res.render("add");
 })
+
+app.post('/posts', async (req,res)=>{
+    await Blog.create(req.body);
+    res.redirect("/");
+})
+
 
 
 
