271b6d4408c485d878c0104231e8e7c55ecc0e47
Anılcan Yurtbay <anilcanyurtbay@hotmail.com>
2021-11-01 13:13:14 +0300

    partials folder structure & route settings

--- nodejs/week2/clean-blog/app.js
+++ nodejs/week2/clean-blog/app.js
@@ -11,8 +11,13 @@ app.use(express.static('public'))
 
 // ROUTES
 app.get('/',(req,res)=>{
-    
     res.render("index");
+})
+app.get('/about',(req,res)=>{
+    res.render("about");
+})
+app.get('/add',(req,res)=>{
+    res.render("add");
 })
 
 
